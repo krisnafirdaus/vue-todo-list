eb370b26402594d3c45aa039ce3a85d3648bd942
Krisna Firdaus <krisnafirdaus@gmail.com>
2023-12-09 14:56:31 +0700

    initial project

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script>
+  import ItemComponent from './components/ItemComponent.vue';
+  import TodoList from './components/TodoList.vue';
+
+  export default {
+    components: {
+      ItemComponent,
+      TodoList
+    },
+  data() {
+    return {
+      items: [
+        {
+          id: 1,
+          text: "item 1"
+        },
+        {
+          id: 2,
+          text: "item 2"
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<template>
+  <!-- <ul>
+    <ItemComponent v-for="item in items" :key="item.id" :item="item" />
+  </ul> -->
+  <TodoList />
+</template>
+
+<style scoped>
+body {
+  color: black;
+}
+</style>
---
+++ src/components/MyComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Haii</h1>
+</template>
+
+<script>
+import MyComponent from './MyComponent.vue';
+
+export default {
+    components: {
+        MyComponent
+    }
+}
+</script>
